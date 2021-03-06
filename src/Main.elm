9400775096990ed0af0a245ab8b43b9fe42995c2
Forrest Cinelli <forrestccinelli@gmail.com>
2021-03-06 10:43:32 -0500

    show details support

--- src/Main.elm
+++ src/Main.elm
@@ -14,9 +14,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, img, text, p)
 import Html.Events exposing (onClick)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (style, attribute)
 import List exposing (head, length, tail, map)
 import Maybe
 import Random
@@ -27,15 +27,16 @@ type alias Model = { star: Star, innerZone: List PlanetaryFeature, habitableZone
 
 ----
 
-type Star = M | G | F | B
-stars = [M, G, F, B]
+type Star = M | G | F | B | VII
+stars = [M, G, F, B, VII]
 
 regionSizes: Star -> { inner: Int, habitable: Int, outer: Int }
 regionSizes star = case star of 
-    M -> { inner = 3, habitable = 3, outer = 5 } -- red.
-    G -> { inner = 1, habitable = 5, outer = 3 } -- yellow. the Sun
-    F -> { inner = 3, habitable = 3, outer = 3 } -- white.
-    B -> { inner = 5, habitable = 1, outer = 3 } -- blue.
+    VII -> { inner = 0, habitable = 1, outer = 5 }
+    M -> { inner = 3, habitable = 3, outer = 5 }
+    G -> { inner = 1, habitable = 5, outer = 3 }
+    F -> { inner = 3, habitable = 3, outer = 3 }
+    B -> { inner = 5, habitable = 1, outer = 3 }
 
 showStar: Star -> String
 showStar star = case star of 
@@ -43,9 +44,10 @@ showStar star = case star of
     G -> "Yellow Dwarf"
     F -> "Yellow-White Star"
     B -> "Blue Giant"
+    VII -> "White Dwarf"
 
 --RockyPlanet (List (Maybe OrbitalFeature)) | GasGiant (List (Maybe PlanetaryFeature)) | AsteroidBelt | AsteroidCluster
-type PlanetaryFeature = RockyPlanet | GasGiant | AsteroidBelt | AsteroidCluster | DustCloud | GravityRiptide | RadiationBurst | SolarFlare | DerelictStation | StarshipGraveyard
+type PlanetaryFeature = RockyPlanet | GasGiant | AsteroidBelt | AsteroidCluster | DustCloud | GravityRiptide | RadiationBurst | SolarFlare | DerelictStation | StarshipGraveyard StarshipGraveyardOrigin
 type TerrestrialPlanet = TerrestrialPlanet PlanetBody PlanetGravity Atmosphere Temperature (List OrbitalFeature)
 type GiantPlanet = GiantPlanet GasBody GasGravity (List OrbitalFeature)
 
@@ -59,6 +61,8 @@ type Temperature = Burning | Hot | Temperate | Cold | Ice
 
 type Atmosphere = None | Thin AtmosphereComposition | Moderate AtmosphereComposition | Heavy AtmosphereComposition
 type AtmosphereComposition = Deadly | Corrosive | Toxic | Tainted | Pure
+
+type StarshipGraveyardOrigin = CrushedFleet | FleetEngagement | LostExplorers | PlunderedConvoy | Skirmish | Unknown
 
 tmpPlanet = TerrestrialPlanet LowMass Low None Burning []
 tmpGiant = GiantPlanet Dwarf Weak []
@@ -75,7 +79,17 @@ showPlanet p = case p of
     RadiationBurst -> "Radiation Burst"
     SolarFlare -> "Solar Flare"
     DerelictStation -> "Derelict Station"
-    StarshipGraveyard -> "Starship Graveyard"
+    StarshipGraveyard o -> "Starship Graveyard"
+
+showGraveyardOrigin: StarshipGraveyardOrigin -> String
+showGraveyardOrigin o = "Origin: " ++ (case o of
+        CrushedFleet -> "Crushed Defence Force/Routed Invasion"
+        FleetEngagement -> "Fleet Engagement"
+        LostExplorers -> "Lost Explorers"
+        PlunderedConvoy -> "Plundered Convoy"
+        Skirmish -> "Skirmish"
+        Unknown -> "Unknown Provenance"
+    )
 
 type OrbitalFeature = Moon
 
@@ -126,7 +140,7 @@ randomHabitable = (Random.int 21 100) |> Random.andThen (\roll ->
         else if roll <= 58 then Random.constant DustCloud
         else if roll <= 64 then Random.constant GravityRiptide
         else if roll <= 93 then randomRocky
-        else                    Random.constant StarshipGraveyard
+        else                    Random.map (\o -> StarshipGraveyard o) randomGraveyardOrigin
     )
 
 randomOuter: Random.Generator PlanetaryFeature
@@ -138,8 +152,10 @@ randomOuter =  (Random.int 21 100) |> Random.andThen (\roll ->
         else if roll <= 73 then randomGas
         else if roll <= 80 then Random.constant GravityRiptide
         else if roll <= 93 then randomRocky
-        else                    Random.constant StarshipGraveyard
+        else                    Random.map (\o -> StarshipGraveyard o) randomGraveyardOrigin
     )
+
+
 
 randomRocky: Random.Generator PlanetaryFeature--TerrestrialPlanet
 randomRocky = Random.constant RockyPlanet
@@ -147,6 +163,15 @@ randomRocky = Random.constant RockyPlanet
 randomGas: Random.Generator PlanetaryFeature--GiantPlanet
 randomGas = Random.constant GasGiant
 
+randomGraveyardOrigin: Random.Generator StarshipGraveyardOrigin
+randomGraveyardOrigin = (Random.int 1 100) |> Random.andThen (\roll ->
+        if roll <= 15 then      Random.constant CrushedFleet
+        else if roll <= 20 then Random.constant FleetEngagement
+        else if roll <= 35 then Random.constant LostExplorers
+        else if roll <= 65 then Random.constant PlunderedConvoy
+        else if roll <= 90 then Random.constant Skirmish
+        else                    Random.constant Unknown
+    )
 
 ----
 
@@ -162,12 +187,24 @@ system {star, innerZone, habitableZone, outerZone} =
         ((starView star) :: (innerView innerZone) ++ (habitableView habitableZone) ++ (outerView outerZone))
 
 starView: Star -> Html Msg
-starView star = div starStyle [ (text (showStar star)) ]
+starView star = div starStyle 
+    [ img (starImg star) []
+    , (text (showStar star))
+    ]
+
+starImg star = (case star of
+    VII -> attribute "src" "file:///C:/Users/Forre/elm/soi-gen/WhiteDwarf.jpg"
+    M -> attribute "src" "file:///C:/Users/Forre/elm/soi-gen/RedDwarf.jpg"
+    G -> attribute "src" "file:///C:/Users/Forre/elm/soi-gen/YellowDwarf.jpg"
+    F -> attribute "src" "file:///C:/Users/Forre/elm/soi-gen/YellowWhiteDwarf.jpg"
+    B -> attribute "src" "file:///C:/Users/Forre/elm/soi-gen/BlueGiant.gif"
+    ) :: (style "width" "100%") :: (style "height" "80%") :: []
 
 starStyle: List (Html.Attribute msg)
 starStyle = 
     [ style "padding-top" "0.9%"
     , style "padding-left" "0.9%"
+    , style "padding-right" "0.9%"
     , style "font-weight" "bold"
     ]
 
@@ -179,13 +216,13 @@ planetContainerStyle =
     ]
 
 innerView: List PlanetaryFeature -> List (Html Msg)
-innerView = map (\p -> div (planetStyle "#EEB0B0") [ text (showPlanet p) ])
+innerView = map (\p -> div (planetStyle "#EEB0B0") [ planetView p ])
 
 habitableView: List PlanetaryFeature -> List (Html Msg)
-habitableView = map (\p -> div (planetStyle "#B0EEB0") [ text (showPlanet p) ])
+habitableView = map (\p -> div (planetStyle "#B0EEB0") [ planetView p ])
 
 outerView: List PlanetaryFeature -> List (Html Msg)
-outerView = map (\p -> div (planetStyle "LightSteelBlue") [ text (showPlanet p) ])
+outerView = map (\p -> div (planetStyle "LightSteelBlue") [ planetView p ])
 --i for names
 planetStyle: String -> List (Html.Attribute msg)
 planetStyle color = 
@@ -196,8 +233,22 @@ planetStyle color =
     , style "padding-top" "0.9%"
     , style "padding-left" "0.9%"
     , style "font-weight" "bold"
+    , style "font-size" "medium"
     ]
 
+planetView: PlanetaryFeature -> Html Msg
+planetView planet = case planet of
+    StarshipGraveyard o -> div [] [ text (showPlanet planet), p planetDetailStyle [ text (showGraveyardOrigin o) ] ]
+    x -> text (showPlanet x)
+
+
+
+planetDetailStyle: List (Html.Attribute msg)
+planetDetailStyle = 
+    [ style "font-weight" "normal"
+    , style "font-style" "italic"
+    , style "font-size" "small"
+    ]
 --
 
 generateButton = div []
@@ -209,7 +260,7 @@ buttonStyle: List (Html.Attribute msg)
 buttonStyle = 
     [ style "width" "300px"
     , style "margin-top" "10px"
-    , style "font-size" "16px"
+    , style "font-size" "18px"
     , style "border" "none"
     , style "background-color" "#397cd5"
     , style "color" "white"
